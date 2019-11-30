4c3f8b1dd614b67f72fb5458a5ea6bf701060dac
herisvan321 <herisvan321@gmail.com>
2019-11-30 10:36:58 +0700

    test update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
             return back();
         }
 
-
+        $tampil = '';
         $url = $_SERVER['REQUEST_URI'];
         if($url == '/home/pages/menu')
         {
